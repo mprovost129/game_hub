4ce7e28cd3287cc4eb53f74fa01c257a4a7244a1
Michael Provost <mprovost129@gmail.com>
2026-08-24 16:03:40 -0400

    Initial Commit

---
+++ Game Hub/Game_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_HubApp.swift
+//  Game Hub
+//
+//  Created by Michael Provost on 8/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Game_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Game HubTests/Game_HubTests.swift
@@ -0,0 +1,19 @@
+//
+//  Game_HubTests.swift
+//  Game HubTests
+//
+//  Created by Michael Provost on 8/24/26.
+//
+
+import Testing
+@testable import Game_Hub
+
+struct Game_HubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
